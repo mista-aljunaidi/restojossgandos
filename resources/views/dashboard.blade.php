ca4981179a15f2e6d691c256cc8b2a74722e6206
NAMEV2023 <achmad.23172@mhs.unesa.ac.id>
2025-10-07 15:53:53 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,16 +22,10 @@
       <form action="{{ route('logout') }}" method="POST">
         @csrf
         <button type="submit"
-          class="flex items-center justify-center bg-gradient-to-r from-red-500 to-red-700 text-white p-2 rounded-lg shadow hover:opacity-90 transition">
-          <svg xmlns="http://www.w3.org/2000/svg" 
-              fill="none" 
-              viewBox="0 0 24 24" 
-              stroke-width="1.8" 
-              stroke="currentColor" 
-              class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" 
-                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6A2.25 2.25 0 005.25 5.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M18 12H9m9 0l-3 3m3-3l-3-3" />
-          </svg>
+          class="flex items-center justify-center bg-gradient-to-r from-red-500 to-red-700 text-white p-3 rounded-xl shadow-lg hover:opacity-90 transition transform hover:scale-105">
+          <img src="{{ asset('img/exit.png') }}" 
+              alt="Logout" 
+              class="w-8 h-8 invert" />
         </button>
       </form>
     </div>
